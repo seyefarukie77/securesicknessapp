e42c89edc67bae8975bad493bc6e247952236f7b
seyefarukie77 <seyefarukie@yahoo.com>
2026-03-28 22:36:53 +0000

    Add Kubernetes provider and update GKE settings

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,10 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 5.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
   }
 }
 
@@ -15,72 +19,30 @@ provider "google" {
   zone    = var.zone
 }
 
-# Artifact Registry (existing)
 resource "google_artifact_registry_repository" "app_images" {
   location      = var.region
   repository_id = "app-images"
   format        = "DOCKER"
-
-  # Keep description to avoid Terraform removing it
-  description = "App images"
+  description   = "App images"
 }
 
 data "google_client_config" "default" {}
 
-provider "kubernetes" {
-  host                   = google_container_cluster.gke.endpoint
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(google_container_cluster.gke.master_auth[0].cluster_ca_certificate)
-}
-
-# GKE Cluster (existing)
 resource "google_container_cluster" "gke" {
   name     = "my-gke-cluster"
   location = var.zone
 
-  # DO NOT set initial_node_count for imported clusters
-  # DO NOT set remove_default_node_pool
-  # DO NOT set addons_config, logging_config, monitoring_config, etc.
+  remove_default_node_pool = true
+  initial_node_count       = 1
 
   networking_mode = "VPC_NATIVE"
 
-  # Use empty block to match existing cluster
   ip_allocation_policy {}
 
-  # Match real cluster network
   network    = "default"
   subnetwork = "default"
-
-  # Prevent Terraform from trying to recreate cluster
- lifecycle {
-  ignore_changes = [
-    node_config,
-    node_pool,
-    addons_config,
-    logging_config,
-    monitoring_config,
-    master_auth,
-    cluster_autoscaling,
-    resource_labels,
-    authenticator_groups_config,
-    gateway_api_config,
-    identity_service_config,
-    mesh_certificates,
-    database_encryption,
-    default_snat_status,
-    enable_intranode_visibility,
-    enable_tpu,
-    enable_autopilot,
-    logging_service,
-    monitoring_service,
-    node_locations,
-    node_version,
-    ip_allocation_policy,
-  ]
-}
 }
 
-# Node Pool (existing)
 resource "google_container_node_pool" "primary_nodes" {
   name     = "default-pool"
   location = var.zone
@@ -95,13 +57,15 @@ resource "google_container_node_pool" "primary_nodes" {
       "https://www.googleapis.com/auth/cloud-platform",
     ]
   }
+}
 
-  lifecycle {
-    ignore_changes = [
-      node_config,
-      version,
-      upgrade_settings,
-      management,
-    ]
-  }
+provider "kubernetes" {
+  host                   = google_container_cluster.gke.endpoint
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.gke.master_auth[0].cluster_ca_certificate)
+
+  depends_on = [
+    google_container_cluster.gke,
+    google_container_node_pool.primary_nodes,
+  ]
 }
