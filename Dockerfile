7d3a328ee9d9bae493d1e2e5e6cd749e797f51b4
seyefarukie77 <seyefarukie@yahoo.com>
2026-04-01 21:58:40 +0000

    Update app

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,6 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-EXPOSE 8000
+EXPOSE 8080
 
-CMD ["gunicorn", "-b", "0.0.0.0:8000", "app:app"]
+CMD ["gunicorn", "-b", "0.0.0.0:${PORT}", "--workers=2", "--threads=4", "app:app"]
--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,16 +1,24 @@
 variable "project_id" {
+  description = "GCP project ID"
   type        = string
   default     = "able-cogency-250511"
 }
 
 variable "region" {
+  description = "GCP region"
   type        = string
   default     = "europe-west1"
 }
 
 variable "zone" {
+  description = "GCP zone"
   type        = string
   default     = "europe-west1-b"
+}
+
+variable "image_repo" {
+  description = "Artifact Registry image repository"
+  type        = string
 }
 
 variable "image_tag" {
